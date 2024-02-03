4c0f5ddd840c1e33b435d6a1e79271c1c9943153
FarahOurir <154355525+FarahOurir@users.noreply.github.com>
2024-02-03 15:55:57 +0100

    Update drag_drop_bot.py

--- drag_drop_bot.py
+++ drag_drop_bot.py
@@ -7,16 +7,15 @@ def move_files(source_folder, destination_pathway):
             for root_dest, dirs_dest, files_dest in os.walk(destination_pathway):
                 destination_file_path = os.path.join(root_dest,"configs", file_name)
 
-                # Check if the file already exists in the destination folder
+                
                 if os.path.exists(destination_file_path):
                     print("exists")
-                    # Replace the file in the destination folder
                     shutil.move(os.path.join(source_folder, file_name), destination_file_path)
                     print(f"Replaced '{file_name}' in the destination folder.")
-                    break  # Stop searching for the file in other subfolders
+                    break  
         
 
-# Replace these paths with your actual folder paths
+
 source_folder_path = "/Users/farah/OneDrive/Bureau/projet_gns3/configs"
 destination_pathway_path = "/Users/farah/GNS3/projects/gns_test/project-files/dynamips"
 
